ed8186c7c84a8da073a558dd60fe5553aa8b5a6c
Willem Meints <willem.meints@gmail.com>
2017-12-30 03:21:27 +0100

    Disable browsable API docs
    We have swagger docs that are way better.

--- recipebot/settings.py
+++ recipebot/settings.py
@@ -122,3 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# REST Framework configuration
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
